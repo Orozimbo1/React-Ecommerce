a31ad988b0c1c2862dea787484c31e0133fd23b4
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-29 21:33:03 -0300

    Buscando na Sanity os dados

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,25 +1,41 @@
 import React from 'react'
 
+// Sanity
+import { client } from '../lib/client'
+
 // Componets
 import { HeroBanner, FooterBanner, Product } from '@/components'
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-
+      <HeroBanner HeroBanner={bannerData} />
       <div className='products-heading'>
         <h2>Best Seller Products</h2>
         <p>Speaker of many variations</p>
       </div>
 
       <div className='products-container'>
-        {['Prod 1', 'Prod 2'].map((product) => <Product key={product} />)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  // Products
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  // Banner
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
