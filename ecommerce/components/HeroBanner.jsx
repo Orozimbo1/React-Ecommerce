27553351e752f1e9f7710baf1798e101e0802ef3
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-30 21:01:33 -0300

    Consumindo os dados do sanity na pagina de produtos

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 
 import { urlFor } from '@/lib/client'
+import product from '@/sanity_ecommerce/schemas/product'
 
 const HeroBanner = ({ heroBanner }) => {
   return (
@@ -11,7 +12,7 @@ const HeroBanner = ({ heroBanner }) => {
         <p className='beats-solo'>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image' />
+        <img src={urlFor(heroBanner.image)} alt={heroBanner.product} className='hero-banner-image' />
         <div>
           <Link href={`/product/${heroBanner.product}`}>
             <button type='button'>{heroBanner.buttonText}</button>
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -16,10 +16,10 @@ const Home = ({ products, bannerData }) => {
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner fotterBanner={bannerData && bannerData[0]} />
     </>
   )
 }
