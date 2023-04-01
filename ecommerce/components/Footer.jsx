45a42c0eb6e71359cba052136c5be75544842db3
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-31 21:33:01 -0300

    Estrutura do Footer

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>&copy; 2023 KaBrum All rigths reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
