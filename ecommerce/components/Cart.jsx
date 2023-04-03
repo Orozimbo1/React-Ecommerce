3bcecc8af9b1e967cec25dbcad5f1cdeafbf1c13
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-03 20:17:25 -0300

    Logica e estrutura jsx de carrinho sem itens

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Link from 'next/link'
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai'
+import { TiDeleteOutline } from 'react-icons/ti'
+import { toast } from 'react-hot-toast'
+
+import { useStateContext } from '@/context/StateContext'
+import { urlFor } from '@/lib/client'
 
 const Cart = () => {
+  const cartRef = useRef()
+  const { setShowCart, cartItems, totalPrice, totalQuantities } = useStateContext()
+
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className='heading'>Your cart</span>
+          <span className='cart-num-items'>({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className='empty-cart'>
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href='/'>
+              <button type='button' className='btn' onClick={() => setShowCart(false)}>
+                Continue shopping
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
