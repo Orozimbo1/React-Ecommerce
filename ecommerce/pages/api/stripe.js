a7d244c433a9db879b1778906df10e6a498c39f7
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-06 19:59:38 -0300

    Criando a página de sucesso

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -15,6 +15,8 @@ export default async function handler(req, res) {
           { shipping_rate: 'shr_1MtfelGJDhmosrbqBdlVMAdf' }
         ],
         line_items: req.body.map((item) => {
+          
+          // olhar com calma pois a imagem não carrega
           const img = item.image[0].asset._ref
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/607pd0oq/production/').replace('webp', '.webp')
 
